14de287aefb9ea4164d0b868b0d3865ae3c6cdbf
gjl <1079603003@qq.com>
2021-06-28 19:22:04 +0800

    add UpdHaoYou

---
+++ logic/UpdHaoYou.go
@@ -0,0 +1,39 @@
+package logic
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func UpdHaoYou(c *gin.Context) {
+	hyId := c.DefaultPostForm("hyId", "0")
+	hystatu, _ := strconv.Atoi(c.DefaultPostForm("hyId", "0"))
+	log.Printf("UpdHaoYou hyId:%s hystatu:%d\n", hyId, hystatu)
+
+	qqhys := qqhy{}
+	result := db.Find(&qqhys, "hy_id = ?", hyId)
+	if result.Error != nil {
+		log.Println("UpdHaoYou ERROR")
+		log.Println(result.Error)
+		c.JSON(http.StatusOK, gin.H{
+			"result": 0,
+		})
+		return
+	}
+
+	qqhys.HyStatu = hystatu
+	result = db.Save(&qqhys)
+	if result.Error != nil {
+		log.Println("UpdHaoYou ERROR")
+		log.Println(result.Error)
+		c.JSON(http.StatusOK, gin.H{
+			"result": 0,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"result": 1,
+		})
+	}
+}
